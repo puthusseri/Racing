e127edf10eb73e80a6ea0851f0ab567ea4320186
puthusseri <vyshakputhusseri@gmail.com>
2018-11-08 17:55:57 +0530

    Added a new scene

---
+++ Assets/Scripts/mainmenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class mainmenu : MonoBehaviour {
+
+	public void playgame() {
+		
+		SceneManager.LoadScene("terrain");
+	}
+	public void playgameold() {
+		
+		SceneManager.LoadScene("TextTrack");
+	}
+	public void quitbutton() {
+		Debug.Log("quit");
+		Application.Quit();
+	}
+}
